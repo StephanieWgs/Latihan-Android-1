7e963d203bfe7731cb455f6824708a2d78a6b2f5
StephanieWgs <126796594+StephanieWgs@users.noreply.github.com>
2024-01-17 11:17:00 +0700

    Latihan-Android-1

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Latihan Android 1"
+include(":app")
+ 
